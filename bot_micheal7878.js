eb803bf642b7dd26f5777dfa45192d667c1889da
Federico Di Gesù <federicodigesu@MacBook-Pro-di-Federico.local>
2018-03-02 13:26:16 +0100

    Grosso aggiornamento. Sistemato RT-di-RT, commenti, e auto-RT del BOT. Aggiunto vecchi user.

--- bot_micheal7878.js
+++ bot_micheal7878.js
@@ -4,6 +4,11 @@ var Twit = require('twit');
 
 var nacho = require('./Key_micheal7878.js');
 
+var fs = require('fs')
+var logger = fs.createWriteStream('report_Nacho.txt', {
+  flags: 'a' // 'a' means appending (old data will be preserved)
+});
+
 var T = new Twit(nacho);
 
 console.log('Log-in avvenuto correttamente.');
@@ -12,67 +17,72 @@ function BOMBA(tweet) {
 	// grab ID of tweet to retweet
 	var tweetId = tweet.id_str;
 	var tweetName = tweet.user.name;
+	var testoTweet = tweet.text;
+	var inRisposta = tweet.in_reply_to_status_id_str;
+	console.log(testoTweet);
 
-
-	if (tweet.user.id == sevengoldenhorses || tweet.user.id == bitconius || tweet.user.id == dbrain || tweet.user.id == doctailor || tweet.user.id == familypoints || tweet.user.id == iconic || tweet.user.id == kora || tweet.user.id == mossland || tweet.user.id == stealthcrypto ) {
-		//funzione Retweetta e metti mi piace
-		function RTF(){
-			
-			// fai retweet
-			console.log('facendo il retweet');
-
-			T.post('statuses/retweet/:id', { id: tweetId }, function (err, data, response) {
-				if (err){
-					console.log('errore');
-				}else{
-					console.log('RT fatto a '+ tweetName);
-				}
-			});
-			//metti mi piace
-			console.log('Mettendo mi piace');
-
-			T.post('favorites/create', { id: tweetId }, function (err, data, response) {
-				if (err){
-					console.log('errore');
-				}else{
-					console.log('LK messo a '+ tweetName);
-				}
-			});
-		};	
-
-		//creo un nemro casuale da moltiplicare per fare il ritardo in millisecondi
-		var r = Math.floor(Math.random() * 30) + 1;
-		
-		var rr = r*60000+900000; //r x 1 minuto + 15min
-		var tempoRitardo = rr/60000;
-		//fammi vedere quanto tempo si è creato
-		//console.log(r + ' per il RT di '+ tweetName); 
-		//console.log(rr + ' per il RT di '+ tweetName);
-		console.log(tempoRitardo + ' minuti al RT e LK di ' + tweetName );
+	if ( tweet.user.screen_name !== 'Micheal7878' ) { 	
+		if( testoTweet.includes('RT ') == false && inRisposta === null ){
+			// Fai la funzione Retweetta e metti mi piace
+			function RTF(){
+				// fai retweet
+				T.post('statuses/retweet/:id', { id: tweetId }, function (err, data, response) {
 					
-		setTimeout(RTF, rr );
+					if (err){
+						console.log('Errore.');
+					}else{
+						console.log('RT fatto a '+ tweetName);
+					}			
+				});
+				//metti mi piace
+				T.post('favorites/create', { id: tweetId }, function (err, data, response) {
+					if (err){
+						console.log('errore');
+					}else{
+						console.log('LK messo a '+ tweetName);
+					}
+				});
+				//scrivi report
+				logger.write('https://twitter.com/castellodocet/status/'+ tweet.id_str + ' creato il ' + tweet.created_at + "\n");
+				console.log('Report fatto.');
+			}; //fine funzione RTF
 
-	};
-};
+			//creo un nemro casuale da moltiplicare per fare il ritardo in millisecondi
+			var r = Math.floor(Math.random() * 10) + 1;
+			var rr = r*1000; //r x 1 secondo
+			var tempoRitardo = rr/1000;
+			console.log(tempoRitardo + ' secondo/i al RT e LK di ' + tweetName );
+			//faccio partire la funzione con ritardo
+			setTimeout(RTF, rr );
+		}else{
+		 	console.log('E\' un RT o un commento. Non lo retwitto.');
+		}; //chiusura IF RT o COMMENTO
+	}else{
+		console.log('E\' un RT del BOT. Non fare niente.' );
+	}; //chiusura IF utente bot
+}; //chiusura di BOMBA
 
 //insert ID user account da seguire (esempio MorpheusNetwork, Vestarin). 
-//Find it easy http://gettwitterid.com/?user_name=castellodocet&submit=GET+USER+ID
-//var castellodocet = '726776221';
-//var micheal7878 = '909704040380010501';
-var sevengoldenhorses = '920917613655584768'; //tengo questo account per fare dei test di controllo
 var bitconius = '953264830454095872';
 var dbrain = '888326326284890113';
 var doctailor = '944605766404763649';
 var familypoints = '957565773387616256';
 var iconic = '950095434952921090';
 var kora = '904533387729358854';
-var mossland = '948119493040943104';
 var skychain = '935520196865019905';
+var mossland = '948119493040943104';	
 var stealthcrypto = '955280820058509312';
+//sotto vecchi
+var dubtokens = '900442763598192641';
+var jointeamwell = '3731754254';
+var morphcrypto = '943369448035049472';
+var farmatrust = '887638060116082688';
+var vestarin = '928665122645692416';
+var medichainonline = '754008453979439105';
 
 
 //parte la connessione con 
-var stream = T.stream('statuses/filter', { follow:  ( ' 920917613655584768 , 953264830454095872 , 888326326284890113 , 944605766404763649 , 957565773387616256 , 950095434952921090 , 904533387729358854 , 948119493040943104 , 935520196865019905 , 955280820058509312 ' ) });
+var stream = T.stream('statuses/filter', { follow:  ( ' 953264830454095872 , 888326326284890113 , 944605766404763649 , 957565773387616256 , 950095434952921090 , 904533387729358854 , 935520196865019905, 948119493040943104 ,   , 955280820058509312, 900442763598192641 , 3731754254 , 943369448035049472 ,  887638060116082688 , 928665122645692416 , 754008453979439105 ' ) });
 console.log('Connesisone al utente avvenuta correttamente.');
 //ogni volta che l'account twitta lui fa partire la funzione Bomba
 stream.on('tweet', BOMBA );
