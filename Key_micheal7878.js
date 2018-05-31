6ee450d75ccf150b697d58cf120eab34c9f49581
3d4K9ZlAUUic <33258239+SevenGoldenHorses@users.noreply.github.com>
2018-05-31 11:39:30 +0200

    Update Key_micheal7878.js

--- Key_micheal7878.js
+++ Key_micheal7878.js
@@ -1,8 +1,8 @@
 module.exports = {
   consumer_key:         'urR9MDnIUzgxPGeOaAeNk4ouQ',
   consumer_secret:      'qdFcik12oDcp6EXlp3oV62MsYXV4pUiL6N9C7AtoFm6PAS2gJZ',
-  access_token:         '909704040380010501-Et4m6fwL74hZl5iy7UApjISIsu7i7nX',
-  access_token_secret:  '1as49HCxboOxpYqdOt8tULwgMItjtmV5h0F3FE9ZPIodg',
+  access_token:         '909704040380010501-l0PQvXv8TvHRwt8fKmmanGM9EGMwXbJ',
+  access_token_secret:  'p2HmkwxTZATgWCADFgfw0ktFDeVAv0EAOnDqJaEoClJZS',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
 
